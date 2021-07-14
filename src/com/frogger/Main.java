32f4b83b2f5de2cd7c3c258c9adf7c9839f0c3dc
dancarreon <daniel.carreon@clevertech.biz>
2021-07-14 20:16:47 +0200

    FROG-15

--- src/com/frogger/Main.java
+++ src/com/frogger/Main.java
@@ -3,6 +3,7 @@ package com.frogger;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("Hello Universe");
     }
 }
