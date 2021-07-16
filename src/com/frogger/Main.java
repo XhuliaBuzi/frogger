923a34d2d4800005a1bafa8928322d9357b60501
Xhulia <xhuliabuzzi@gmail.com>
2021-07-16 12:18:13 +0200

    Hello Daniel

--- src/com/frogger/Main.java
+++ src/com/frogger/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello Fellow Developers!");
+        System.out.println("Daniel: -Hello Fellow Developers!");
+        System.out.println("Julie: -Hello Daniel!");
     }
 }
