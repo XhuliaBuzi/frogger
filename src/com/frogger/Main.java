a7d0f8f5d264ad4392ae52ffd7c35ebea379bf4f
Xhulia <xhuliabuzzi@gmail.com>
2021-07-26 10:52:55 +0200

    some new update

--- src/com/frogger/Main.java
+++ src/com/frogger/Main.java
@@ -3,13 +3,9 @@ package com.frogger;
 public class Main {
 
     public static void main(String[] args) {
-//        JPanel panel=new JPanel();
-//        panel.setSize(40,40);
-//        panel.setToolTipText("hey");
-//        panel.setDebugGraphicsOptions(JFrame.EXIT_ON_CLOSE);
-//        panel.setVisible(true);
 
 Border border=new Border();
+
     }
 
 }
