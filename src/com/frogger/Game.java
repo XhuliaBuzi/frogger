0dd57f72a9d4c3d8e5b3a767caa5aa195a559abf
Xhulia <xhuliabuzzi@gmail.com>
2021-07-26 12:17:41 +0200

    frog 12

--- src/com/frogger/Game.java
+++ src/com/frogger/Game.java
@@ -2,7 +2,7 @@ package com.frogger;
 
 public class Game {
     public int[] boardSize=new int[40];
-    public int[] roadSize=new int[(boardSize.length)/4];
+    public int[] roadSize=new int[10];
     public int[] riverSize=new int[(boardSize.length)/4];
 
 
