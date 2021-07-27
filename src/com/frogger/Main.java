04e98385bfe4603bc2c3f0e405039930394a8158
Xhulia <xhuliabuzzi@gmail.com>
2021-07-27 10:29:49 +0200

    Frog 31

--- src/com/frogger/Main.java
+++ src/com/frogger/Main.java
@@ -1,11 +1,28 @@
 package com.frogger;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public Main() {
+        JFrame frame = new JFrame();
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.black);
 
-        Border border = new Border();
-        System.out.println("Frog");
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 800, 800);
+        panel.setLayout(null);
+
+
+    }
+
+
+    public static void main(String[] args) {
+        new Main();
     }
 
 }
