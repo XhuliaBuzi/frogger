b6da5d28930ddb2df1682161a20413a3f8bc2976
Xhulia <xhuliabuzzi@gmail.com>
2021-07-28 12:13:24 +0200

    Frog 31

--- src/com/frogger/Game.java
+++ src/com/frogger/Game.java
@@ -12,9 +12,39 @@ public class Game extends JPanel {
     public int[][] boardSize = new int[Size][Size];
     public int[] roadSize = new int[10];
     public int[] riverSize = new int[10];
-    Color color;
-
-
+//
+    public Game(ImageIcon i,int x,int y){
+        this.xloc=x;
+        this.yloc=y;
+        this.i=i;
+    }
+    private JLabel movment;
+private int xloc,yloc,speed;
+private  ImageIcon i;
+private final int width=16,height=16;
+public int getX(){
+    return this.xloc;
+}
+public  int getY(){
+    return this.yloc;
+}
+public int getSpeed(){
+    return this.speed;
+}
+public JLabel getMovment(){
+    return  this.movment;
+}
+public void setX(int x){
+    this.xloc=x;
+}
+    public void setY(int y){
+        this.yloc=y;
+    }
+public void setupMovment(){
+   movment=new JLabel();
+    movment.setBounds(this.xloc,this.yloc,width,height);
+movment.setIcon(this.i);
+}
     public Game() {
 
     }
--- src/com/frogger/Main.java
+++ src/com/frogger/Main.java
@@ -6,23 +6,11 @@ import java.awt.Color;
 
 public class Main {
 
-    public Main() {
-        JFrame frame = new JFrame();
-        frame.setSize(800, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.black);
-
-        JPanel panel = new JPanel();
-        panel.setBounds(0, 0, 800, 800);
-        panel.setLayout(null);
-
-
-    }
-
-
     public static void main(String[] args) {
-        new Main();
+
+        MyBoard form=new MyBoard();
+        form.setVisible(true);
+        form.setSize(800,800);
     }
 
 }
